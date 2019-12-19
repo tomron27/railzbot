3168a042bf096a87618a540a74b3ff049d7d23ae
tomron27 <tomron27@gmail.com>
2019-12-19 21:16:30 +0200

    Updates

--- bot.py
+++ bot.py
@@ -217,7 +217,7 @@ if __name__ == '__main__':
                               url_path=TOKEN)
         updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
-        app_url = "https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN)
+        app_url = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
         wakeup_wrapper(app_url)
 
     else:
