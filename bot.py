073d2fc82a7c52ee2231d05d2ae1e573c7671c3b
tomron27 <tomron27@gmail.com>
2019-12-19 21:01:15 +0200

    Updates

--- bot.py
+++ bot.py
@@ -218,7 +218,7 @@ if __name__ == '__main__':
         updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
         app_url = "localhost"
-        wakeup_wrapper(app_url, PORT)
+        wakeup_wrapper(app_url)
 
     else:
         logger.error("No MODE specified!")
